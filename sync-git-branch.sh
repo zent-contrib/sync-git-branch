6e9aff7f2c370878ee5b025b43f7b83bb7cf8204
Estevão Costa <estevao90@gmail.com>
2022-08-09 22:14:26 -0300

    Allow multiple branches

--- sync-git-branch.sh
+++ sync-git-branch.sh
@@ -6,16 +6,13 @@ SOURCE_REPO="$1"
 SOURCE_BRANCH="$2"
 DESTINATION_REPO="$3"
 DESTINATION_BRANCH="${4:-$2}"
-REPO_DIR="repo"
 
 echo "SOURCE=$SOURCE_REPO:$SOURCE_BRANCH"
 echo "DESTINATION=$DESTINATION_REPO:$DESTINATION_BRANCH"
 
-# dir cleanup
-rm -fr "$REPO_DIR"
-
-# Only clone the branch we want to sync
-git clone --single-branch --branch "$SOURCE_BRANCH" "$SOURCE_REPO" "$REPO_DIR"
-cd "$REPO_DIR"
 git remote add mirror "$DESTINATION_REPO"
+
+# Pull all branches references down locally so subsequent commands can see them
+git fetch source '+refs/heads/*:refs/heads/*' --update-head-ok
+
 git push -f mirror "$SOURCE_BRANCH:$DESTINATION_BRANCH"
