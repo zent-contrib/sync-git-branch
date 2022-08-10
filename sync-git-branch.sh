2ff3b4d0c855c06ddefa16e6db9ab401ff4e468e
Estevão Costa <estevao90@gmail.com>
2022-08-09 22:22:31 -0300

    Fix issues

--- sync-git-branch.sh
+++ sync-git-branch.sh
@@ -6,13 +6,15 @@ SOURCE_REPO="$1"
 SOURCE_BRANCH="$2"
 DESTINATION_REPO="$3"
 DESTINATION_BRANCH="${4:-$2}"
+REPO_DIR="repo"
 
 echo "SOURCE=$SOURCE_REPO:$SOURCE_BRANCH"
 echo "DESTINATION=$DESTINATION_REPO:$DESTINATION_BRANCH"
 
-git remote add mirror "$DESTINATION_REPO"
+rm -fr "$REPO_DIR"
+git clone "$SOURCE_REPO" "$REPO_DIR" --origin source && cd "$REPO_DIR"
 
-# Pull all branches references down locally so subsequent commands can see them
+git remote add mirror "$DESTINATION_REPO"
 git fetch source '+refs/heads/*:refs/heads/*' --update-head-ok
 
 git push -f mirror "$SOURCE_BRANCH:$DESTINATION_BRANCH"
